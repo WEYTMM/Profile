dc9338b3078ed568bc2e3bdf501c7e33ec31c295
WEYT.MM <60260203+WEYTMM@users.noreply.github.com>
2020-01-30 22:45:48 +0700

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,75 @@
+# My Profile
+# Coded by WEYT.MM
+# Mau Ngapain ster?
+# Kaya gini gampang nya masa mau di recode juga:v
+
+
+
+
+import os
+import sys
+import httplib
+import socket
+from time import sleep
+
+
+
+
+
+
+os.system ("clear")
+os.system ("figlet Hai User")
+sleep (2)
+os.system ("clear")
+print ("")
+print ("")
+print ("")
+os.system ("figlet Introduce")
+print ("Hi, My Name is Fadhli")
+print ("I Live In Bogor")
+print ("I Don't Like You, But")
+print ("You Is Gay")
+print ("n Do You Know, What Is Gay?")
+print ("Please Search on Google, Keyword: 'Gay In The World Only One, This is Me' ")
+print ("Done?")
+print ("Please Check Now!")
+sleep (20)
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+os.system ("clear")
+print ("Done?")
+print ("You Know Gay?")
+print ("Yea,")
+sleep (7)
+os.system ("clear")
+print ("You.")
+print ("Good.")
+print ("You Now is Gay Only One in The World")
+print ("Congrulations For You, But You Is Gay")
+sleep (5)
+os.system ("clear")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+os.system ("figlet Thansk For Introduce")
+sleep (4)
+os.system ("clear")
+print ("")
+print ("")
+print ("")
+print ("Oiya btw Gw lupa wkwk:v")
+sleep (2)
+os.system ("clear")
+print ("Follow me: https://fb.me/WEYT.MM")
+print ("Thanks:*")
+sleep (5)
+
+
+
+
+
