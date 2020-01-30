b3782fa7d4d9abbb3b32478c1df83c5ef6372a6d
WEYT.MM <60260203+WEYTMM@users.noreply.github.com>
2020-01-30 22:09:58 +0700

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import httplib
+import socket
+from time import sleep
+
+
+
+class bcolors:
+	"""docstring for colors"""
+	HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+
+
+
+os.system ("clear")
+os.system ("figlet Hai User")
+sleep (2)
+os.system ("clear")
+print ("")
+print ("")
+print ("")
+os.system ("figlet Introduce")
+print bcolors.RED ("Hi, My Name is Fadhli")
+print bcolors.RED ("I Live In Bogor")
+print bcolors.RED ("I Don't Like You, But")
+print bcolors.RED ("You Is Gay")
+print bcolors.HEADER ("n Do You Know, What Is Gay?")
+print bcolors.GREEN ("Please Search on Google, Keyword: 'Gay In The World Only One, This is Me' ")
+print bcolors.RED ("Done?")
+print bcolors.RED ("Please Check Now!")
+sleep (3)
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+os.system ("clear")
+print bcolors.ENDC ("Done?")
+print bcolors.ENDC ("You Know Gay?")
+print bcolors.ENDC ("Yea,")
+sleep (2)
+print bcolors.ENDC ("You.")
+print bcolors.ENDC ("Good.")
+print bcolors.ENDC ("You Now is Gay Only One in The World")
+print bcolors.ENDC ("Congrulations For You, But You Is Gay")
+sleep (3)
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+os.system ("figlet Thansk For Introduce")
+sleep (2)
+print ("")
+print ("")
+print ("")
+print bcolors.YELLOW ("Oiya btw Gw lupa wkwk:v")
+sleep (2)
+os.system ("clear")
+print bcolors.BLUE ("Follow me: https://fb.me/WEYT.MM")
+print bcolors.BLUE ("Thanks:*")
+sleep (5)
+
+
+
+
+
